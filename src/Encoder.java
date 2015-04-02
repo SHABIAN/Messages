6da8a5b706ffff588320d8375244367c594bb13c
SHABIAN <rishab.pandian@gmail.com>
2015-04-02 13:53:32 -0500

    Encoder
    needs to be finised

--- src/Encoder.java
+++ src/Encoder.java
@@ -1,4 +1,20 @@
+import java.io.File;
+import java.util.List;
 
-public class Encoder {
+
+public class Encoder 
+{
+	public Encoder(File File)
+	{
+		
+	}
+	public void write(String plaintext)
+	{
+		
+	}
+	public List<String> read();
+	{
+		
+	}
 
 }
