47a533b71552ef7f71f62e4d9bdee568becf5b0a
SHABIAN <rishab.pandian@gmail.com>
2015-04-02 13:46:41 -0500

    real messages
    stuff

--- src/Messages.java
+++ src/Messages.java
@@ -1,4 +1,23 @@
+import java.util.ArrayList;
 
-public class Messages {
+
+public class Messages 
+{
+	ArrayList<String> mess;
+	String us;
+	public Messages(String username, ArrayList<String> Messages)
+	{
+		us = username;
+		mess = Messages;
+		
+	}
+	public String getUser()
+	{
+		return us;
+	}
+	public ArrayList<String> getMessages()
+	{
+		return mess;
+	}
 
 }
