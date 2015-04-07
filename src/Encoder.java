5238507e264503eb92f4a94e8d86c0fc19e8fed4
SHABIAN <rishab.pandian@gmail.com>
2015-04-07 13:42:07 -0500

    Encoder
    Ecoder

--- src/Encoder.java
+++ src/Encoder.java
@@ -1,20 +1,55 @@
 import java.io.File;
-import java.util.List;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 
 public class Encoder 
 {
+	File f;
+//	ArrayList<Messages> m;
 	public Encoder(File File)
 	{
-		
+		f= File;
 	}
 	public void write(String plaintext)
 	{
+		try {
+			PrintWriter p = new PrintWriter(plaintext);
+			p.print(plaintext);
+			p.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 	}
-	public List<String> read()
+	public ArrayList<Messages> read()
 	{
-		return null;
+		ArrayList<Messages> m = new ArrayList<Messages>();
+
+		try{
+		Scanner fin = new Scanner(f);
+		while(fin.hasNextLine()){
+			String[] s = fin.nextLine().split("\t");
+			String user = s[0];
+			ArrayList<String> mes = new ArrayList<String>();
+			for (int i= 1; i<s.length;i++)
+			{
+				mes.add(s[i]);
+			}
+			m.add(new Messages(user,mes));
+			
+			
+			
+		}
+		} catch(FileNotFoundException e){
+			e.printStackTrace();
+		}
+		return m;
 	}
+	
+
 
 }
